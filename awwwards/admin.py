bf1ec92b60ed256f8eb90c1ada0a1c391ab230f9
Steve <mittosteve@gmail.com>
2019-10-19 01:43:27 +0300

    Creating the profile model and professions model

--- awwwards/admin.py
+++ awwwards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Profile, Profession
+
+
+admin.site.register(Profile)
+admin.site.register(Profession)
 # Register your models here.
--- awwwards/models.py
+++ awwwards/models.py
@@ -1,1 +1,33 @@
-from django.db import models
+from django.db import models as md
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+class Profession(md.Model):
+    profession = md.CharField(max_length=100)
+
+    class Meta:
+        ordering=['profession']
+
+    def __str__(self):
+        return f'{self.profession}'
+class Profile(md.Model):
+    user = md.OneToOneField(User,on_delete=md.PROTECT)
+    profile_pic= md.ImageField(upload_to='profile_pics/')
+    bio = md.TextField(max_length = 300)
+    display_name = md.CharField(max_length = 100)
+    profession = md.ManyToManyField(Profession)
+    email=md.EmailField(max_length=255)
+    created_on = md.DateField(auto_now_add = True)
+
+    @receiver(post_save, sender = User)
+    def create_profile(sender,instance,created ,**kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender = User)
+    def save_profile(sender, instance,**kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return f'{self.user.username}'
