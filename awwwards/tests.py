c3a3647792932ef79a48c70429a07c3129fe1d84
Steve <mittosteve@gmail.com>
2019-10-19 23:21:24 +0300

    Testting user creation profile

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from . models import Country,Tag,Post,Rating,Follow,Like,Profile,Profession
 
 class ProfessionTest(TestCase):
+    new_profess= Profession(profession = 'Web Developer')
     def setUp(self):
         self.new_profess= Profession(profession = 'Web Developer')
 
@@ -15,6 +16,7 @@ class ProfessionTest(TestCase):
         self.assertTrue(all_professions,1)
 
 class CountryTest(TestCase):
+    new_country= Country(name = 'Web Developer')
     def setUp(self):
         self.new_country= Country(name = 'Web Developer')
 
@@ -25,3 +27,19 @@ class CountryTest(TestCase):
         self.new_country.save_country()
         all_professions= Country.objects.all().count()
         self.assertTrue(all_professions,1)
+
+
+class ProfileTest(ProfessionTest,CountryTest,TestCase):
+    def setUp(self):
+        user = User(username = "John" , password="1q3e5t7u")
+        user.save()
+        country=CountryTest.new_country
+        country.save_country()
+        profession=ProfessionTest.new_profess
+        profession.save_professions()
+        self.new_profile = Profile.objects.filter(user=user).first()
+        # self.new_profile.profession.set(profession)
+        # self.new_profile.country.set(country)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
