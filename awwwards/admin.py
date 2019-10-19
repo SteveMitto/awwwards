a44e69b401f8303600f96e98fa394e6ac0f16deb
Steve <mittosteve@gmail.com>
2019-10-19 22:10:01 +0300

    This commit registers all my models in the admin site

--- awwwards/admin.py
+++ awwwards/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Profile, Profession
+from .models import Profile, Profession ,Country, Tag, Post, Rating, Follow, Like
 
 
 admin.site.register(Profile)
 admin.site.register(Profession)
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Rating)
+admin.site.register(Follow)
+admin.site.register(Like)
