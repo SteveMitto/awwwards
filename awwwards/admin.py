c8ad67bd9cc5287fd7ce392a08b362fab77d993e
Steve <mittosteve@gmail.com>
2019-10-22 03:48:32 +0300

    Registering contributors and site of the day to the admin

--- awwwards/admin.py
+++ awwwards/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Image ,Profile, Profession ,Country, Tag, Post, Rating, Follow, Like
+from .models import Image ,Contributor ,Profile, Profession ,Country, Tag, Post, Rating, Follow, Like,Sotd
 
 
 admin.site.register(Image)
@@ -9,6 +9,8 @@ admin.site.register(Profession)
 admin.site.register(Country)
 admin.site.register(Tag)
 admin.site.register(Post)
+admin.site.register(Contributor)
 admin.site.register(Rating)
 admin.site.register(Follow)
 admin.site.register(Like)
+admin.site.register(Sotd)
